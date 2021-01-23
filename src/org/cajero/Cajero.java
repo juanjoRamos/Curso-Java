524e9a94a7bc3588ada7cab7a5fa83506b487c96
juanjoRamos <jjramosmartinez96@gmail.com>
2021-01-23 14:56:05 +0100

    Retocado el método principal para que solo se pueda acceder al cajero si tu usuario existe previamente en un vector de Cuentas.

--- src/org/cajero/Cajero.java
+++ src/org/cajero/Cajero.java
@@ -3,6 +3,7 @@
  */
 package org.cajero;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -26,50 +27,76 @@ public class Cajero {
 		Scanner sc = new Scanner(System.in);
 		int numeroIntroducido = -1;
 		float dinero = -1;
-		Cuenta c1 = new Cuenta("juanjoRamos", 0f);
+		String nombre = null;
+		Cuenta persona = null;
 
-		do {
-			System.out.println(
-					"---------- Operaciones ---------- \n0. Cerrar consulta. \n1. Consultar saldo. \n2. Ingresar dinero. \n3. Retirar dinero. \n---------------------------------\nIntroduzca el valor: ");
-			try {
-				numeroIntroducido = sc.nextInt();
-				switch (numeroIntroducido) {
-				case 0:
-					break;
-				case 1:
-					System.out.println("Tienes un total de " + c1.getSalario() + "€");
-					Thread.sleep(2 * 1000);
-					break;
-				case 2:
-					System.out.print("Cantidad a ingresar: ");
-					dinero = sc.nextFloat();
-					System.out.println("Dinero en la cuenta: " + c1.getSalario() + " €. Ingresados: " + dinero + "€");
-					c1.ingresar(dinero);
-					Thread.sleep(2 * 1000);
-					break;
-				case 3:
-					System.out.print("Cantidad a retirar: ");
-					dinero = sc.nextFloat();
-					System.out.println("Dinero en la cuenta: " + c1.getSalario() + " €. Retirados: " + dinero + "€");
-					c1.retirar(dinero);
-					Thread.sleep(2 * 1000);
-					break;
-				default:
-					System.out.println("Debe introducir un valor entre el 0 al 3.");
-					Thread.sleep(2 * 1000);
-					break;
-				}
-			} catch (InputMismatchException e) {
-				System.out.println("El valor introducido no es correcto.");
-				Thread.sleep(2 * 1000);
-				/* Cuando el usuario introduzca un valor no entero mandara el mensaje y el
-				 escaner no lo "tomara en cuenta" comenzará de nuevo.*/
-				sc.nextLine();
+		ArrayList<Cuenta> listaCuenta = new ArrayList<Cuenta>();
+		listaCuenta.add(new Cuenta("juanjoRamos", 0f));
+		System.out.println(listaCuenta.toString());
+		System.out.print("Introduzca su nombre: ");
+		nombre = sc.nextLine();
+		System.out.print("Introduzca su número secreto: ");
+		numeroIntroducido = sc.nextInt();
+
+		for (int i = 0; i < listaCuenta.size(); i++) {
+			if (listaCuenta.get(i).getNombreTitular().equals(nombre)
+					&& (listaCuenta.get(i).getNumeroSecreto() == numeroIntroducido)) {
+				numeroIntroducido = -1;
+				persona = new Cuenta(listaCuenta.get(i).getNombreTitular(), listaCuenta.get(i).getSalario());
 			}
-		} while (numeroIntroducido != 0);
-		sc.close();
-		System.out.println("Gracias.");
+		}
 
+		if (persona != null) {
+			do {
+				System.out.println(
+						"---------- Operaciones ---------- \n0. Cerrar consulta. \n1. Consultar saldo. \n2. Ingresar dinero. \n3. Retirar dinero. \n---------------------------------\nIntroduzca el valor: ");
+				try {
+					numeroIntroducido = sc.nextInt();
+					switch (numeroIntroducido) {
+					case 0:
+						break;
+					case 1:
+						System.out.println("Tienes un total de " + persona.getSalario() + "€");
+						Thread.sleep(2 * 500);
+						break;
+					case 2:
+						System.out.print("Cantidad a ingresar: ");
+						dinero = sc.nextFloat();
+						System.out.println(
+								"Dinero en la cuenta: " + persona.getSalario() + " €. Ingresados: " + dinero + "€");
+						persona.ingresar(dinero);
+						Thread.sleep(2 * 500);
+						break;
+					case 3:
+						System.out.print("Cantidad a retirar: ");
+						dinero = sc.nextFloat();
+						System.out.println(
+								"Dinero en la cuenta: " + persona.getSalario() + " €. Retirados: " + dinero + "€");
+						persona.retirar(dinero);
+						Thread.sleep(2 * 500);
+						break;
+					default:
+						System.out.println("Debe introducir un valor entre el 0 al 3.");
+						Thread.sleep(2 * 500);
+						break;
+					}
+				} catch (InputMismatchException e) {
+					System.out.println("El valor introducido no es correcto.");
+					Thread.sleep(2 * 500);
+					/*
+					 * Cuando el usuario introduzca un valor no entero mandara el mensaje y el
+					 * escaner no lo "tomara en cuenta" comenzará de nuevo.
+					 */
+					sc.nextLine();
+				}
+			} while (numeroIntroducido != 0);
+			sc.close();
+			System.out.println("Gracias por usar mi cajero.");
+		} else {
+			System.out.println("Alguno de los datos introducidos no es correcto. Intentelo mas tarde.");
+		}
+		
+		
 	}
 
 }
